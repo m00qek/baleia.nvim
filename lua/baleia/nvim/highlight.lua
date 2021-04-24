916662a3635d7cf21d416f9489a5278dcfb31288
António Mora <m00qek@criptext.com>
2021-04-24 15:49:26 -0300

    Fix highlight position

--- lua/baleia/nvim/highlight.lua
+++ lua/baleia/nvim/highlight.lua
@@ -3,41 +3,40 @@ local END_OF_LINE = -1
 local module = {}
 
 function module.create(name, attributes)
-  local command = 'highlight ' .. name
+   local command = 'highlight ' .. name
 
-  if attributes.cterm then
+   if attributes.cterm then
       command = command .. ' cterm=' .. table.concat(attributes.cterm, ',')
-  end
+   end
 
-  attributes.cterm = nil
-  for attr, value in pairs(attributes) do
-    if value then
-      command = command .. ' ' .. attr .. '=' .. value
-    end
-  end
+   attributes.cterm = nil
+   for attr, value in pairs(attributes) do
+      if value then
+         command = command .. ' ' .. attr .. '=' .. value
+      end
+   end
 
-  return vim.api.nvim_command(command)
+   return vim.api.nvim_command(command)
 end
 
 function module.one(buffer, ns, highlight)
-  local lastcolumn = END_OF_LINE
-  if highlight.lastcolumn then
-    lastcolumn = highlight.lastcolumn - 1
-  end
-
-  return vim.api.nvim_buf_add_highlight(buffer, ns, highlight.name,
-    highlight.line - 1, highlight.firstcolumn - 1, lastcolumn)
+   return vim.api.nvim_buf_add_highlight(
+      buffer,
+      ns,
+      highlight.name,
+      highlight.line - 1,
+      highlight.firstcolumn - 1,
+      highlight.lastcolumn or END_OF_LINE)
 end
 
 function module.all(buffer, ns, definitions, highlights)
-  for _, definition in ipairs(definitions) do
-    module.create(definition.name, definition.attributes)
-  end
+   for _, definition in ipairs(definitions) do
+      module.create(definition.name, definition.attributes)
+   end
 
-  for _, highlight in ipairs(highlights) do
-    module.one(buffer, ns, highlight)
-  end
+   for _, highlight in ipairs(highlights) do
+      module.one(buffer, ns, highlight)
+   end
 end
 
 return module
-
