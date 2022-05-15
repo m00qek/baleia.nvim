d4fe7b0905e6bf66e1447a291441f0720ddfa279
António Mora <m00qek@gmail.com>
2022-05-15 11:08:08 -0300

    Better folder structure

--- test/spec/baleia/text_spec.lua
+++ test/spec/baleia/text_spec.lua
@@ -1,6 +1,7 @@
 local text = require('baleia.text')
 
 describe("[strip_color_codes]", function()
+
   it("strip", function()
     local lines = { "first \x1b[31mline", "second \x1b[32mline\x1b[0m", "third line" }
     assert.combinators.match(
