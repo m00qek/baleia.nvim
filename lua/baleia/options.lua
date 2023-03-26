fb3aff021b2b64ef820d0230d2c22ebfaf71bb6a
António Mora <m00qek@gmail.com>
2023-03-27 00:17:17 +0200

    Fix bug in config to not strip codes. Closes #12

--- lua/baleia/options.lua
+++ lua/baleia/options.lua
@@ -6,15 +6,24 @@ local options = {}
 function options.with_default(opts)
   opts = opts or { }
 
-  local loglevel = opts.log == true and 'ERROR' or opts.log
-
-  return {
-    strip_ansi_codes = opts.strip_ansi_codes or true,
-    line_starts_at = opts.line_starts_at or 1,
+  local final_opts = {
+    strip_ansi_codes = true,
+    line_starts_at = 1,
     colors = colors.theme(ansi.NR_8),
-    name = opts.name or "BaleiaColors",
-    log = loglevel or false,
+    name = "BaleiaColors",
+    log = false,
   }
+
+  for key, _ in pairs(final_opts) do
+    local user_value = opts[key]
+    if user_value ~= nil then
+      final_opts[key] = user_value
+    end
+  end
+
+  final_opts.loglevel = final_opts.log == true and 'ERROR' or opts.log
+
+  return final_opts
 end
 
 return options
