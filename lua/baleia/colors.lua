f7319895d0a6975212baff9e7bd77e51cda1a952
António Mora <m00qek@gmail.com>
2023-12-20 14:42:18 +0100

    Document xterm

--- lua/baleia/colors.lua
+++ lua/baleia/colors.lua
@@ -1,50 +1,50 @@
-local xterm = require('baleia.colors.xterm')
+local xterm = require("baleia.styles.xterm")
 
 local colors = {}
 
 function colors.none()
-  return {
-    set = false,
-    value = {
-      name = 'none',
-      cterm = 'none',
-      gui = 'none',
-    }
-  }
+	return {
+		set = false,
+		value = {
+			name = "none",
+			cterm = "none",
+			gui = "none",
+		},
+	}
 end
 
 function colors.reset()
-  return {
-    set = true,
-    value = {
-      name = 'none',
-      cterm = 'none',
-      gui = 'none',
-    }
-  }
+	return {
+		set = true,
+		value = {
+			name = "none",
+			cterm = "none",
+			gui = "none",
+		},
+	}
 end
 
 function colors.from_xterm(code)
-  return {
-    set = true,
-    value = {
-      name = code,
-      cterm = code,
-      inferred = { gui = xterm.to_truecolor(code) }
-    }
-  }
+	return {
+		set = true,
+		value = {
+			name = code,
+			cterm = code,
+			inferred = { gui = xterm.to_truecolor(code) },
+		},
+	}
 end
 
 function colors.from_truecolor(red, green, blue)
-  local hexcode = string.format('%02x%02x%02x', red or 0, green or 0, blue or 0)
-  return {
-    set = true,
-    value = {
-      name = hexcode,
-      cterm = xterm.from_rgb(red, green, blue),
-      gui = '#' .. hexcode,
-    }
-  }
+	local hexcode = string.format("%02x%02x%02x", red or 0, green or 0, blue or 0)
+	return {
+		set = true,
+		value = {
+			name = hexcode,
+			cterm = xterm.from_rgb(red, green, blue),
+			gui = "#" .. hexcode,
+		},
+	}
 end
 
 return colors
