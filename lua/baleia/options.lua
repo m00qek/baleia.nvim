9901b9acf2734d5d58f45332762212c015914959
António Mora <m00qek@gmail.com>
2023-12-22 08:00:02 +0100

    Document user interface

--- lua/baleia/options.lua
+++ lua/baleia/options.lua
@@ -1,37 +1,33 @@
 local themes = require("baleia.styles.themes")
-local log = require("baleia.log")
-
----@class UserOptions
----@field strip_ansi_codes? boolean
----@field line_starts_at? integer
----@field colors? Theme
----@field name? string
----@field log? string
+local logger = require("baleia.logger")
+local nvim = require("baleia.nvim")
 
 ---@class Options
 ---@field strip_ansi_codes boolean
 ---@field line_starts_at integer
+---@field namespace integer
 ---@field log_level string
 ---@field colors Theme
----@field logger any
+---@field logger Logger
 ---@field name string
 
 local options = {}
 
----@param opts UserOptions
+---@param user_options? UserOptions
 ---@return Options
-function options.with_default(opts)
-	opts = opts or {}
+function options.with_defaults(user_options)
+	local opts = user_options or {}
 
-	---@type Options
 	local final_opts = {
 		strip_ansi_codes = true,
 		line_starts_at = 1,
 		log_level = opts.log or "ERROR",
 		colors = themes.with_colorscheme(themes.NR_8),
-		logger = log.NULL_LOGGER,
 		name = "BaleiaColors",
 	}
+
+	final_opts.namespace = nvim.create_namespace(final_opts.name)
+	final_opts.logger = logger.new(final_opts.name .. "Log", final_opts.ns, final_opts.log_level)
 
 	for key, _ in pairs(final_opts) do
 		local user_value = opts[key]
